a61dc4c1af3632e7740b4ac6f02bde71d5fc4d03
Lex <vasickin@mail.ru>
2024-01-01 13:43:29 +0300

    created a repository on GitHub

--- task_1.cs
+++ task_1.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите
+// программу, которая выведет все натуральные числа
+// в промежутке от M до N. Использовать рекурсию, не
+// использовать циклы.
+
+Console.WriteLine("Введите число для M");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число для N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void NumbersInRange(int M, int N)
+{
+    if (M > N)
+    {
+        return;
+    }
+
+    Console.Write(M + " ");
+    NumbersInRange(M + 1, N);
+}
+
+Console.Write("Числа от M до N: ");
+NumbersInRange(M, N);
+Console.WriteLine();
